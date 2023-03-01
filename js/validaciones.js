8b2f345061cc0f29480b2283681f2383535ee966
crypzus <mejiasernestor@gmail.com>
2023-03-01 12:51:26 -0500

    clase05 terminada

--- js/validaciones.js
+++ js/validaciones.js
@@ -3,7 +3,7 @@ export function validar(input) {
   if (validadores[tipoDeInput]) {
     validadores[tipoDeInput](input);
   }
-  console.log (input.parentElement);
+ 
   if (input.validity.valid){
       input.parentElement.classList.remove("input-container--invalid");
       input.parentElement.querySelector(".input-message-error").innerHTML = "";
@@ -28,11 +28,27 @@ const mensajeDeError = {
     },
     password: {
         valueMissing : "la contraseña no esta llena ",
-        patterMismarch : "Mín 6 max 12 caract.,1 letra mayúscula, 1 letra minúscula y 1 número"
+        patternMismatch : "Mín 6 max 12 caract.,1 letra mayúscula, 1 letra minúscula y 1 número"
     },
     nacimiento: {
         valueMissing : "falta agregar la fecha vacio",
         custtomError : "debes tener al menos 18 años de edad"
+    },
+    numero:{
+        valueMissing : "el campo esta  vacio",
+        patternMismatch : "el formato requerido es XXXXXXXXX"
+    },
+    direccion: {
+        valueMissing : "campo esta vacio ",
+        patternMismatch : "la direcion debe contener min entre 4 a 15 catracteres"
+    },
+    ciudad: {
+        valueMissing : "campo esta vacio ",
+        patternMismatch : "el estado  debe contener min entre 4 a 15 catracteres"
+    },
+    estado: {
+        valueMissing : "campo esta vacio ",
+        patternMismatch : "el estado  debe contener min entre 4 a 15 catracteres"
     },
 }
  
@@ -40,7 +56,6 @@ function mostrarMensajeDeError (tipoDeInput, input){
     let mensaje = "";
     tiposDeErrores.forEach(error => {
         if (input.validity[error]){
-           
             console.log(tipoDeInput,error); 
             console.log(input.validity[error]);
             console.log(mensajeDeError[tipoDeInput][error]);
